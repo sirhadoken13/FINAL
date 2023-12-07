bcc6db1edce13bb83c70657b228413603d104a7c
sirhadoken13 <2281036@unai.edu>
2023-12-08 03:17:45 +0700

    content finish

---
+++ bootstrap/script.js
@@ -0,0 +1,13 @@
+function topFunction() {
+    document.body.scrollTop = 0; // Untuk Safari
+    document.documentElement.scrollTop = 0; // Untuk Chrome, Firefox, IE, dan Opera
+  }
+
+  // Menunjukkan atau menyembunyikan tombol berdasarkan posisi scroll
+  window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("backToTopBtn").style.display = "block";
+    } else {
+      document.getElementById("backToTopBtn").style.display = "none";
+    }
+  };
